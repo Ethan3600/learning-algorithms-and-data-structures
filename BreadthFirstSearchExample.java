42ac51c20cba0ba4d9f8ff1b08878f3576799a4a
Ethan3600 <Ethan3600@gmail.com>
2017-06-05 17:19:27 -0400

    adding bidirectional search algorithm

--- BreadthFirstSearchExample.java
+++ BreadthFirstSearchExample.java
@@ -49,4 +49,49 @@ public class BreadthFirstSearchExample
 			}
 		}
 	}
+	
+	public static void Bfs(Node root, char destination)
+	{
+		Queue<Node> queue = new LinkedList<Node>(); 
+		root.visited = true;
+		queue.add(root);
+		
+		while(!queue.isEmpty())
+		{
+			Node n = queue.remove();
+			n.visited = true;
+			
+			for(Node r : n.adjacent)
+			{
+				if(r.visited == false)
+				{
+					r.visited = true;
+					r.parent = n;
+					
+					if(r.key == destination)
+					{
+						// returns parent path to root
+						String parentPath = calcParentPath(r);
+						System.out.println(parentPath);
+					}
+					
+					queue.add(r);
+				}
+			}
+		}
+		
+		// couldn't find destination
+	}
+
+	private static String calcParentPath(Node r)
+	{
+		Node p = r.parent;
+		String path = r.key + " ";
+		while(p != null)
+		{
+			path += p.key + " ";
+			p = p.parent;
+		}
+		return path;
+	}
 }
